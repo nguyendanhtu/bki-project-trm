72a934d75b9473f8f1a0967c6372a521d37d2dc9
khanh020991@gmail.com <khanh020991@gmail.com@90c61b1b-a1e5-0917-55b7-fb95f9a1af4d>
2012-05-30 17:31:38 +0000

    load data lên combo đơnvị thanh toán trongform F106 danh mục hồ sơ

--- trunk/03. SourceCode/TRMProject/DanhMuc/F106_DanhMucHoSo.aspx.cs
+++ trunk/03. SourceCode/TRMProject/DanhMuc/F106_DanhMucHoSo.aspx.cs
@@ -14,6 +14,7 @@ public partial class DanhMuc_F106_DanhMucHoSo : System.Web.UI.Page
         if (!IsPostBack)
         {
             load_data_2_combo_giang_vien();
+            load_data_2_combo_don_vi_thanh_toan();
         }
     }
 
@@ -29,5 +30,22 @@ public partial class DanhMuc_F106_DanhMucHoSo : System.Web.UI.Page
 
         m_cbo_giang_vien.DataSource = v_ds_dm_giang_vien.V_DM_GIANG_VIEN;
         m_cbo_giang_vien.DataBind();
+    }
+
+  // load dữ liệu lên combobox đơn vị thanh toán
+    private void load_data_2_combo_don_vi_thanh_toan()
+    {
+        US_DM_DON_VI_THANH_TOAN v_dm_don_vi_thanh_toan = new US_DM_DON_VI_THANH_TOAN();
+        DS_DM_DON_VI_THANH_TOAN v_ds_dm__don_vi_thanh_toan = new DS_DM_DON_VI_THANH_TOAN();
+        v_dm_don_vi_thanh_toan.FillDataset(v_ds_dm__don_vi_thanh_toan);
+
+        m_cbo_don_vi_thanh_toan.DataSource = v_ds_dm__don_vi_thanh_toan.DM_DON_VI_THANH_TOAN;
+
+        m_cbo_don_vi_thanh_toan.DataTextField = DM_DON_VI_THANH_TOAN.TEN_DON_VI;
+        m_cbo_don_vi_thanh_toan.DataValueField = DM_DON_VI_THANH_TOAN.ID;
+
+        m_cbo_don_vi_thanh_toan.DataBind();
+
+        
     }
 }
