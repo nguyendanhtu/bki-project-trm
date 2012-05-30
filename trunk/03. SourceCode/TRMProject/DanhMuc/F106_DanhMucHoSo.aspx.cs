8192c0cba8ac1493579625b0b36715bae6bd3365
ninhvh@bkindex.com <ninhvh@bkindex.com@90c61b1b-a1e5-0917-55b7-fb95f9a1af4d>
2012-05-30 17:33:47 +0000

    Load data lên cbo giang vien form F106_DanhMucHoSo

--- trunk/03. SourceCode/TRMProject/DanhMuc/F106_DanhMucHoSo.aspx.cs
+++ trunk/03. SourceCode/TRMProject/DanhMuc/F106_DanhMucHoSo.aspx.cs
@@ -21,15 +21,25 @@ public partial class DanhMuc_F106_DanhMucHoSo : System.Web.UI.Page
     // load dữ liệu lên combobox giảng viên
     private void load_data_2_combo_giang_vien()
     {
-        US_V_DM_GIANG_VIEN v_dm_giang_vien = new US_V_DM_GIANG_VIEN();
-        DS_V_DM_GIANG_VIEN v_ds_dm_giang_vien = new DS_V_DM_GIANG_VIEN();
-        v_dm_giang_vien.FillDataset(v_ds_dm_giang_vien);
+        US_V_DM_GIANG_VIEN v_us_giang_vien = new US_V_DM_GIANG_VIEN();
+        DS_V_DM_GIANG_VIEN v_ds_giang_vien = new DS_V_DM_GIANG_VIEN();
+        try
+        {
+            v_us_giang_vien.FillDataset(v_ds_giang_vien, " ORDER BY HO_VA_TEN_DEM, TEN_GIANG_VIEN");
+            for (int v_i = 0; v_i < v_ds_giang_vien.V_DM_GIANG_VIEN.Rows.Count; v_i++)
+            {
+                m_cbo_giang_vien.Items.Add(new ListItem(v_ds_giang_vien.V_DM_GIANG_VIEN.Rows[v_i][V_DM_GIANG_VIEN.HO_VA_TEN_DEM].ToString().TrimEnd() + " " + v_ds_giang_vien.V_DM_GIANG_VIEN.Rows[v_i][V_DM_GIANG_VIEN.TEN_GIANG_VIEN].ToString(), v_ds_giang_vien.V_DM_GIANG_VIEN.Rows[v_i][V_DM_GIANG_VIEN.ID].ToString()));
+            }
+            //m_cbo_gvien.DataSource = v_ds_giang_vien.V_DM_GIANG_VIEN;
+            //m_cbo_gvien.DataValueField = V_DM_GIANG_VIEN.ID;
+            //m_cbo_gvien.DataTextField = V_DM_GIANG_VIEN.TEN_GIANG_VIEN;
+            //m_cbo_gvien.DataBind();
+        }
+        catch (Exception v_e)
+        {
 
-        m_cbo_giang_vien.DataTextField = V_DM_GIANG_VIEN.TEN_GIANG_VIEN;
-        m_cbo_giang_vien.DataValueField = V_DM_GIANG_VIEN.ID;
-
-        m_cbo_giang_vien.DataSource = v_ds_dm_giang_vien.V_DM_GIANG_VIEN;
-        m_cbo_giang_vien.DataBind();
+            throw v_e;
+        }
     }
 
   // load dữ liệu lên combobox đơn vị thanh toán
